1a6e352f6c5417d7e40bc45ec7aa02faef09b9d1
fomka31 <128593862+fomka31@users.noreply.github.com>
2025-09-30 13:01:45 +1000

    Update lab4.py

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,7 +1,28 @@
 # Task_1
 
-temp = int(input('Введите температуру: '))
-if temp >= 20:
-    print('Кондиционер выключен')
-else:
-    print('Кондиционер включен')
+# temp = int(input('Введите температуру: '))
+# if temp >= 20:
+#     print('Кондиционер выключается...')
+# else:
+#     print('Кондиционер включается...')
+
+# Task_2
+
+# num = int(input('Введите номер месяца: '))
+# if num in [1,2,12]:
+#     print('Сейчас зима')
+# elif num in [3,4,5]:
+#     print('Сейчас весна')
+# elif num in [6,7,8]:
+#     print('Сейчас лeто')
+# elif num in [9,10,11]:
+#     print('Сейчас осень')
+# else:
+#     print("Введён неверный номер месяца")
+
+# Task_3
+
+try:
+    dog_age = int(input())
+except:
+    print()
