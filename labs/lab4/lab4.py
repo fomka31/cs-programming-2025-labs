579dfd21ef3cd0ce1831dad37dbbf5f1f4dd296a
fomka31 <128593862+fomka31@users.noreply.github.com>
2025-10-01 11:04:46 +1000

    Update lab4.py

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -22,7 +22,21 @@
 
 # Task_3
 
-try:
-    dog_age = int(input())
-except:
-    print()
+# dog_age = input()
+
+# try:
+#     dog_age = int(dog_age)
+#     current_age = dog_age
+#     human_age = 0
+#     if dog_age > 22 or dog_age < 1:
+#         print('Введён некорректный возраст')
+#     else:
+#         while current_age != 0:
+#             if dog_age - current_age < 2:
+#                 human_age += 10.5
+#             else:
+#                 human_age += 4
+#             current_age -= 1
+#         print(human_age)
+# except:
+#     print('введено не число')
