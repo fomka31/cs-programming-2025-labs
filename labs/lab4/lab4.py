7949c782709d42c73948991f454ae616b32501fd
fomka31 <128593862+fomka31@users.noreply.github.com>
2025-10-30 20:33:32 +1000

    Update lab4.py

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -96,7 +96,11 @@
 
 #Task 7
 
-a = [int(i) for i in input('Введите 3 числа :' ).split()]
-minimal = a[0]
-for int(i) in a[1:]:
-    minimal = int(i) if int(i) < minimal else minimal
+# a = [int(i) for i in input('Введите 3 числа :' ).split()]
+# minimal = a[0]
+# for i in a[1:]:
+#     minimal = int(i) if int(i) < minimal else minimal
+# print(minimal)
+
+#Task 8
+
