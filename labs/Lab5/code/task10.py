1d35c0d41bbe43656789b08fc827bf9a268e2005
fomka31 <128593862+fomka31@users.noreply.github.com>
2025-12-02 12:30:21 +1000

    /

--- labs/Lab5/code/task10.py
+++ labs/Lab5/code/task10.py
@@ -4,8 +4,10 @@ stud_list = [("Анна", [5, 4, 5]), ("Иван", [3, 4, 4]), ("Мария", [5
 
 stud_dict = dict()
 
-for x,y in stud_list:
-    stud_dict[x] = (sum(i for i in y) / len(y)).__round__(3)
+for name,grade in stud_list:
+    stud_dict[name] = (sum(i for i in grade) / len(grade)).__round__(3)
 
-prime_char = sorted(stud_dict, key=lambda x : x[1])[0]
-print(f'Лучший ученик : {prime_char}, его(её) средний балл : {stud_dict[prime_char]}')
+sorted_students = sorted(stud_dict.items(), key=lambda student: student[1], reverse=True)
+best_student_name, best_student_score = sorted_students[0]
+
+print(f'Лучший ученик: {best_student_name}, его(её) средний балл: {best_student_score}')
